78768ce6c84724169e84918387f8caca32f624b7
Balavardhan Katta <balavardhankatta01@gmail.com>
2026-08-03 20:49:16 +0530

    creating soft-delete entity and id autoincrement

--- src/main/java/com/springboot/Basic_CRUD_App/Entity/Student.java
+++ src/main/java/com/springboot/Basic_CRUD_App/Entity/Student.java
@@ -1,17 +1,21 @@
 package com.springboot.Basic_CRUD_App.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String mail;
     private String gender;
     private String stream;
+    private boolean deleted;
 
     public int getId() {
         return id;
@@ -52,5 +56,13 @@ public class Student {
 
     public void setStream(String stream) {
         this.stream = stream;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
     }
 }
